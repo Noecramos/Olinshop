75b1ce99712c8ef1300f83f9fe3f9d74d1ac48f5
Noé Ramos <noecramos@gmail.com>
2026-02-05 18:53:36 -0300

    Fix UI: Increase Stock Toggle Visibility

--- app/components/admin/ProductForm.tsx
+++ app/components/admin/ProductForm.tsx
@@ -420,7 +420,7 @@ export default function ProductForm({ restaurantId, onSave, refreshCategories }:
                                             }
                                         }}
                                     />
-                                    <div className="relative w-7 h-4 bg-gray-200 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-orange-600"></div>
+                                    <div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-orange-600"></div>
                                 </label>
                             </div>
 
